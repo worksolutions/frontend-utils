368527b41df465ffd376b14086218cad70bba23c
edelgarat <edelgarat@yandex.ru>
2021-12-08 15:33:52 +0300

    fix createFileInput function

--- src/files/createFileInput.ts
+++ src/files/createFileInput.ts
@@ -2,26 +2,24 @@ import { AcceptTypes, makeInputAccept } from "./inputAccept";
 
 import { FileInterface } from "../types/FileInterface";
 
-export function createFileInput(multiply?: boolean, acceptTypes?: AcceptTypes[]) {
+export function createFileInput(onChange: (files: File[]) => void, multiply?: boolean, acceptTypes?: AcceptTypes[]) {
   const input = document.createElement("input");
   input.type = "file";
   input.style.display = "none";
   input.multiple = !!multiply;
   if (acceptTypes) input.accept = makeInputAccept(acceptTypes);
   document.body.appendChild(input);
+
+  const listener = () => {
+    if (input.files!.length === 0) return;
+    onChange([...input.files!]);
+  };
+
+  input.addEventListener("change", listener);
+
   return {
-    open: (onChange: (files: File[]) => void) => {
-      const listener = () => {
-        if (input.files!.length === 0) return;
-        onChange([...input.files!]);
-        input.removeEventListener("change", listener); // TODO: переписать, так как можно просто закрыть диалог и эта функция не вызовется
-      };
-      input.addEventListener("change", listener);
-      input.click();
-    },
-    destroy: () => {
-      document.body.removeChild(input);
-    },
+    open: () => input.click(),
+    destroy: () => document.body.removeChild(input),
   };
 }
 
