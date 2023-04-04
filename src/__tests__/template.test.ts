ff7d1724735ac3d8280084c512a77a3059010092
agrabarov <agrabarov@broniboy.ru>
2023-04-04 12:33:00 +0300

    update template function

--- src/__tests__/template.test.ts
+++ src/__tests__/template.test.ts
@@ -2,4 +2,5 @@ import { template } from "../template";
 
 test("template replacer test", function () {
   expect(template("hello {num} world", { num: 1 })).toBe("hello 1 world");
+  expect(template("hello %num% world", { num: 1 }, { openingSymbol: "%", closingSymbol: "%" })).toBe("hello 1 world");
 });
--- src/template.ts
+++ src/template.ts
@@ -1,6 +1,17 @@
-export function template(text: string, keyMap: Record<string, string | number>) {
-  return Object.keys(keyMap).reduce(
-    (prev, mapKey) => prev.replace(new RegExp(`{${mapKey}}`, "g"), keyMap[mapKey].toString()),
-    text,
-  );
+export interface TemplateOptionsInterface {
+  openingSymbol: string;
+  closingSymbol: string;
+}
+
+const defaultOptions: TemplateOptionsInterface = { openingSymbol: "{", closingSymbol: "}" };
+
+export function template(
+  text: string,
+  keyMap: Record<string, string | number>,
+  { openingSymbol, closingSymbol } = defaultOptions,
+) {
+  return Object.keys(keyMap).reduce((prev, mapKey) => {
+    const regExp = new RegExp(`${openingSymbol}${mapKey}${closingSymbol}`, "g");
+    return prev.replace(regExp, keyMap[mapKey].toString());
+  }, text);
 }
