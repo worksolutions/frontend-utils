910b8da9a0ab14128e2d03ee515b6a5a75cac583
agrabarov <agrabarov@broniboy.ru>
2024-02-15 16:05:16 +0300

    updates

--- src/memoizeWith.ts
+++ src/memoizeWith.ts
@@ -1,11 +1,11 @@
-export function memoizeWith<T extends (...args: any[]) => unknown>(
-  keyFunction: (...v: Parameters<T>) => string,
-  func: T,
-): T {
-  const cache: Record<string, any> = {};
-  return function (this: any, ...args: Parameters<T>) {
+export function memoizeWith<FUNC extends (...args: any[]) => unknown>(
+  keyFunction: (...v: Parameters<FUNC>) => string | number | symbol,
+  func: FUNC,
+): FUNC {
+  const cache: Record<string | number | symbol, any> = {};
+  return function (this: any, ...args: Parameters<FUNC>) {
     const key = keyFunction.apply(this, args);
     if (!(key in cache)) cache[key] = func.apply(this, args);
     return cache[key];
-  } as T;
+  } as FUNC;
 }
--- src/recursiveFind.ts
+++ src/recursiveFind.ts
@@ -1,12 +1,24 @@
-export function recursiveFind<SUB_ITEMS_FIELD_NAME extends string, ITEM extends Object>(
+export function recursiveFind<
+  SUB_ITEMS_FIELD_NAME extends string,
+  ITEM extends { [key in SUB_ITEMS_FIELD_NAME]?: ITEM[] } & Omit<object, SUB_ITEMS_FIELD_NAME>,
+  INJECT_LEVEL extends boolean = false,
+>(
   subItemsFieldName: SUB_ITEMS_FIELD_NAME,
   criteria: (item: ITEM) => any,
   items: ITEM[],
-): ITEM | undefined {
+  injectLevel?: INJECT_LEVEL,
+  level = 0,
+): (INJECT_LEVEL extends true ? { level: number; item: ITEM } : ITEM) | undefined {
   for (const item of items) {
-    if (criteria(item)) return item;
+    if (criteria(item)) return injectLevel ? { level, item } : (item as any);
     if (subItemsFieldName in item) {
-      const foundInSubItems = recursiveFind(subItemsFieldName, criteria, (item as any)[subItemsFieldName]);
+      const foundInSubItems = recursiveFind(
+        subItemsFieldName,
+        criteria,
+        (item as any)[subItemsFieldName],
+        injectLevel,
+        level + 1,
+      );
       if (foundInSubItems) return foundInSubItems;
     }
   }
