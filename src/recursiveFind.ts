4c70bed6d4ea45986fee4caefade75a8fb963685
agrabarov <agrabarov@broniboy.ru>
2024-09-17 15:17:24 +0300

    update recursiveFind func

--- src/recursiveFind.ts
+++ src/recursiveFind.ts
@@ -11,15 +11,12 @@ export function recursiveFind<
 ): (INJECT_LEVEL extends true ? { level: number; item: ITEM } : ITEM) | undefined {
   for (const item of items) {
     if (criteria(item)) return injectLevel ? { level, item } : (item as any);
-    if (subItemsFieldName in item) {
-      const foundInSubItems = recursiveFind(
-        subItemsFieldName,
-        criteria,
-        (item as any)[subItemsFieldName],
-        injectLevel,
-        level + 1,
-      );
-      if (foundInSubItems) return foundInSubItems;
+    if (subItemsFieldName in item && Array.isArray((item as any)[subItemsFieldName])) {
+      const subItems = (item as any)[subItemsFieldName];
+      if (subItems) {
+        const foundInSubItems = recursiveFind(subItemsFieldName, criteria, subItems, injectLevel, level + 1);
+        if (foundInSubItems) return foundInSubItems;
+      }
     }
   }
 }
